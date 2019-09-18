2c019f04aaa1bb649c8adceeb8b82822808cfefe
harikarthic <m.harikarthic@gmail.com>
2019-09-18 16:19:09 +0200

    fix: resetting blogpost template

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -33,89 +33,89 @@ export const blogQuery =  graphql`
 
 
 
-export default ({data}) => {
-  const post = data.markdownRemark;
-  return (
-    <Layout location="\">
-      <code>{JSON.stringify(post)}</code>
-    </Layout>
-  )
+// export default ({data}) => {
+//   const post = data.markdownRemark;
+//   return (
+//     <Layout location="\">
+//       <code>{JSON.stringify(post)}</code>
+//     </Layout>
+//   )
 
- }
+//  }
 
-// class BlogPostTemplate extends React.Component {
-//   render() {
-//     const post = this.props.data.markdownRemark
-//     const siteTitle = this.props.data.site.siteMetadata.title
-//     const { previous, next } = this.props.pageContext
+class BlogPostTemplate extends React.Component {
+  render() {
+    const post = this.props.data.markdownRemark
+    const siteTitle = this.props.data.site.siteMetadata.title
+    const { previous, next } = this.props.pageContext
 
-//     return (
-//       <Layout location={this.props.location} title={siteTitle}>
-//         <SEO
-//           title={post.frontmatter.title}
-//           description={post.frontmatter.description || post.excerpt}
-//         />
-//         <h1
-//           style={{
-//             marginTop: rhythm(1),
-//             marginBottom: 0,
-//           }}
-//         >
-//           {post.frontmatter.title}
-//         </h1>
-//         <p
-//           style={{
-//             ...scale(-1 / 5),
-//             display: `block`,
-//             marginBottom: rhythm(1),
-//           }}
-//         >
-//           {post.frontmatter.date}
-//         </p>
-//         <p style={{
-//           ...scale(-1 / 5),
-//           display: `block`,
-//           marginBottom: rhythm(1),
-//         }}>{post.frontmatter.tags.join(',')}
-//         </p>
-//         <div dangerouslySetInnerHTML={{ __html: post.html }} />
-//         <hr
-//           style={{
-//             marginBottom: rhythm(1),
-//           }}
-//         />
-//         <Bio />
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO
+          title={post.frontmatter.title}
+          description={post.frontmatter.description || post.excerpt}
+        />
+        <h1
+          style={{
+            marginTop: rhythm(1),
+            marginBottom: 0,
+          }}
+        >
+          {post.frontmatter.title}
+        </h1>
+        <p
+          style={{
+            ...scale(-1 / 5),
+            display: `block`,
+            marginBottom: rhythm(1),
+          }}
+        >
+          {post.frontmatter.date}
+        </p>
+        <p style={{
+          ...scale(-1 / 5),
+          display: `block`,
+          marginBottom: rhythm(1),
+        }}>{post.frontmatter.tags.join(',')}
+        </p>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <hr
+          style={{
+            marginBottom: rhythm(1),
+          }}
+        />
+        <Bio />
 
-//         <ul
-//           style={{
-//             display: `flex`,
-//             flexWrap: `wrap`,
-//             justifyContent: `space-between`,
-//             listStyle: `none`,
-//             padding: 0,
-//           }}
-//         >
-//           <li>
-//             {previous && (
-//               <Link to={previous.fields.slug} rel="prev">
-//                 ← {previous.frontmatter.title}
-//               </Link>
-//             )}
-//           </li>
-//           <li>
-//             {next && (
-//               <Link to={next.fields.slug} rel="next">
-//                 {next.frontmatter.title} →
-//               </Link>
-//             )}
-//           </li>
-//         </ul>
-//       </Layout>
-//     )
-//   }
-// }
+        <ul
+          style={{
+            display: `flex`,
+            flexWrap: `wrap`,
+            justifyContent: `space-between`,
+            listStyle: `none`,
+            padding: 0,
+          }}
+        >
+          <li>
+            {previous && (
+              <Link to={previous.fields.slug} rel="prev">
+                ← {previous.frontmatter.title}
+              </Link>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Link to={next.fields.slug} rel="next">
+                {next.frontmatter.title} →
+              </Link>
+            )}
+          </li>
+        </ul>
+      </Layout>
+    )
+  }
+}
 
-// export default BlogPostTemplate;
+export default BlogPostTemplate;
 
 
 
